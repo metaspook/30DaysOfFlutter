150ee05cc00a8340187958bfa65af1b0af525acf
metaspook <metaspook@gmail.com>
2021-07-03 20:21:06 +0600

    Day-13 completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+  // State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
   final String name = 'AlterLink';
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString('assets/files/catalog.json');
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData['products'];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.white,
         // elevation: 0.0,
         // iconTheme: IconThemeData(color: Colors.black),
-        title: Text(
+        title: const Text(
           'Catalog App',
           // style: TextStyle(
           //   color: Colors.black,
@@ -23,16 +50,20 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) => ItemWidget(
-            item: dummyList[index],
-          ),
-          // itemCount: CatalogModel.items.length,
-          // itemBuilder: (context, index) => ItemWidget(
-          //   item: CatalogModel.items[index],
-          // ),
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+            ? ListView.builder(
+                // itemCount: dummyList.length,
+                // itemBuilder: (context, index) => ItemWidget(
+                //   item: dummyList[index],
+                // ),
+                itemCount: CatalogModel.items!.length,
+                itemBuilder: (context, index) => ItemWidget(
+                  item: CatalogModel.items![index],
+                ),
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
